a2a82a6eba6398835c2a9cbce8ce8b56b6ff9953
katya-sn <kattya.sagun@gmail.com>
2024-05-11 15:54:15 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,25 @@
+"use strict";
+const listOfEmployees = document.querySelector("ul");
+const employees = document.querySelectorAll("li");
+const arrayOfEmployees = [
+    ...employees
+];
+const normalizeSalary = (salary)=>{
+    return Number(salary.slice(1).replace(/,/g, ""));
+};
+while(listOfEmployees.firstChild)listOfEmployees.removeChild(listOfEmployees.firstChild);
+const sortList = ()=>{
+    return arrayOfEmployees.sort((a, b)=>normalizeSalary(b.dataset.salary) - normalizeSalary(a.dataset.salary)).forEach((item)=>listOfEmployees.appendChild(item));
+};
+const getEmployees = ()=>{
+    return arrayOfEmployees.map((employee)=>({
+            name: employee.innerText,
+            position: employee.dataset.position,
+            salary: employee.dataset.salary,
+            age: employee.dataset.age
+        }));
+};
+sortList();
+getEmployees();
+
+//# sourceMappingURL=index.f75de5e1.js.map
